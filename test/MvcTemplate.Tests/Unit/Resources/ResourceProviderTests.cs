f08db025a732cb7b739b3ad8503ccfaf2a00a627
Muchiachio <aldis.rameika@gmail.com>
2015-07-31 20:31:19 +0300

    Resource project tests
    - Added initial port of resources project tests.

---
+++ test/MvcTemplate.Tests/Unit/Resources/ResourceProviderTests.cs
@@ -0,0 +1,240 @@
+﻿using MvcTemplate.Objects;
+using MvcTemplate.Resources;
+using MvcTemplate.Resources.Shared;
+using MvcTemplate.Tests.Objects;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MvcTemplate.Tests.Unit.Resources
+{
+    public class ResourceProviderTests
+    {
+        #region Static method: GetContentTitle(IDictionary<String, Object> values)
+
+        [Fact]
+        public void GetContentTitle_GetsTitleByIgnoringCase()
+        {
+            IDictionary<String, Object> values = new Dictionary<String, Object>();
+            values["area"] = "administration";
+            values["controller"] = "roles";
+            values["action"] = "details";
+
+            String expected = ContentTitles.AdministrationRolesDetails;
+            String actual = ResourceProvider.GetContentTitle(values);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void GetContentTitle_GetsTitleWithoutArea(String area)
+        {
+            IDictionary<String, Object> values = new Dictionary<String, Object>();
+            values["controller"] = "profile";
+            values["action"] = "edit";
+            values["area"] = area;
+
+            String actual = ResourceProvider.GetContentTitle(values);
+            String expected = ContentTitles.ProfileEdit;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetContentTitle_OnTitleNotFoundReturnsNull()
+        {
+            IDictionary<String, Object> values = new Dictionary<String, Object>
+            {
+                ["controller"] = null,
+                ["action"] = null,
+                ["area"] = null
+            };
+
+            Assert.Null(ResourceProvider.GetContentTitle(values));
+        }
+
+        #endregion
+
+        #region Static method: GetSiteMapTitle(String area, String controller, String action)
+
+        [Fact]
+        public void GetSiteMapTitle_GetsTitleByIgnoringCase()
+        {
+            String actual = ResourceProvider.GetSiteMapTitle("administration", "roles", "index");
+            String expected = MvcTemplate.Resources.SiteMap.Titles.AdministrationRolesIndex;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSiteMapTitle_GetsTitleWithoutControllerAndAction()
+        {
+            String actual = ResourceProvider.GetSiteMapTitle("administration", null, null);
+            String expected = MvcTemplate.Resources.SiteMap.Titles.Administration;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSiteMapTitle_OnSiteMapNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetSiteMapTitle("Test", "Test", "Test"));
+        }
+
+        #endregion
+
+        #region Static method: GetPrivilegeAreaTitle(String area)
+
+        [Fact]
+        public void GetPrivilegeAreaTitle_GetsTitleByIgnoringCase()
+        {
+            String expected = MvcTemplate.Resources.Privilege.Area.Titles.Administration;
+            String actual = ResourceProvider.GetPrivilegeAreaTitle("administration");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPrivilegeAreaTitle_OnAreaNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPrivilegeAreaTitle("Test"));
+        }
+
+        [Fact]
+        public void GetPrivilegeAreaTitle_OnNullAreaReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPrivilegeAreaTitle(null));
+        }
+
+        #endregion
+
+        #region Static method: GetPrivilegeControllerTitle(String area, String controller)
+
+        [Fact]
+        public void GetPrivilegeControllerTitle_GetsTitle()
+        {
+            String expected = MvcTemplate.Resources.Privilege.Controller.Titles.AdministrationRoles;
+            String actual = ResourceProvider.GetPrivilegeControllerTitle("Administration", "Roles");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPrivilegeControllerTitle_OnControllerNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPrivilegeControllerTitle("", ""));
+        }
+
+        #endregion
+
+        #region Static method: GetPrivilegeActionTitle(String area, String controller, String action)
+
+        [Fact]
+        public void GetPrivilegeActionTitle_GetsTitle()
+        {
+            String actual = ResourceProvider.GetPrivilegeActionTitle("administration", "accounts", "index");
+            String expected = MvcTemplate.Resources.Privilege.Action.Titles.AdministrationAccountsIndex;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPrivilegeActionTitle_OnActionNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPrivilegeActionTitle("", "", ""));
+        }
+
+        #endregion
+
+        #region Static method: GetPropertyTitle<TModel, TProperty>(Expression<Func<TModel, TProperty>> property)
+
+        [Fact]
+        public void GetPropertyTitle_OnNotMemberExpressionReturnNull()
+        {
+            Assert.Null(ResourceProvider.GetPropertyTitle<TestView, String>(view => view.ToString()));
+        }
+
+        [Fact]
+        public void GetPropertyTitle_GetsTitleFromExpression()
+        {
+            String actual = ResourceProvider.GetPropertyTitle<AccountView, String>(account => account.Username);
+            String expected = MvcTemplate.Resources.Views.AccountView.Titles.Username;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPropertyTitle_GetsTitleFromExpressionRelation()
+        {
+            String actual = ResourceProvider.GetPropertyTitle<AccountEditView, String>(account => account.RoleId);
+            String expected = MvcTemplate.Resources.Views.RoleView.Titles.Id;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPropertyTitle_OnPropertyFromExpressionNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPropertyTitle<AccountView, String>(account => account.Id));
+        }
+
+        [Fact]
+        public void GetPropertyTitle_OnTypeFromExpressionNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPropertyTitle<TestView, String>(test => test.Text));
+        }
+
+        #endregion
+
+        #region Static method: GetPropertyTitle(Type view, String property)
+
+        [Fact]
+        public void GetPropertyTitle_GetsTitleByIgnoringCase()
+        {
+            String actual = ResourceProvider.GetPropertyTitle(typeof(AccountView), "username");
+            String expected = MvcTemplate.Resources.Views.AccountView.Titles.Username;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPropertyTitle_GetsTitleFromRelation()
+        {
+            String actual = ResourceProvider.GetPropertyTitle(typeof(RoleView), "AccountUsername");
+            String expected = MvcTemplate.Resources.Views.AccountView.Titles.Username;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPropertyTitle_GetsTitleFromMultipleRelations()
+        {
+            String actual = ResourceProvider.GetPropertyTitle(typeof(RoleView), "AccountRoleAccountUsername");
+            String expected = MvcTemplate.Resources.Views.AccountView.Titles.Username;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPropertyTitle_OnPropertyNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPropertyTitle(typeof(AccountView), "Id"));
+        }
+
+        [Fact]
+        public void GetPropertyTitle_OnTypeNotFoundReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPropertyTitle(typeof(TestView), "Title"));
+        }
+
+        [Fact]
+        public void GetPropertyTitle_OnNullPropertyKeyReturnsNull()
+        {
+            Assert.Null(ResourceProvider.GetPropertyTitle(typeof(RoleView), null));
+        }
+
+        #endregion
+    }
+}
---
+++ test/MvcTemplate.Tests/Unit/Resources/ResourcesTests.cs
@@ -0,0 +1,103 @@
+﻿using MvcTemplate.Components.Mvc;
+using MvcTemplate.Data.Core;
+using MvcTemplate.Objects;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using Xunit;
+
+namespace MvcTemplate.Tests.Unit.Resources
+{
+    public class ResourcesTests
+    {
+        [Fact]
+        public void Resources_HasAllPrivilegeAreaTitles()
+        {
+            ResourceManager manager = MvcTemplate.Resources.Privilege.Area.Titles.ResourceManager;
+            using (Context context = new Context())
+            {
+                String[] areas = context
+                    .Set<Privilege>()
+                    .Select(priv => priv.Area)
+                    .Distinct()
+                    .ToArray();
+
+                foreach (String area in areas)
+                    Assert.True(!String.IsNullOrEmpty(manager.GetString(area)),
+                        String.Format("Privilege area '{0}', does not have a title.", area));
+            }
+        }
+
+        [Fact]
+        public void Resources_HasAllPrivilegeControllerTitles()
+        {
+            ResourceManager manager = MvcTemplate.Resources.Privilege.Controller.Titles.ResourceManager;
+            using (Context context = new Context())
+            {
+                String[] controllers = context
+                    .Set<Privilege>()
+                    .ToArray()
+                    .Select(priv => priv.Area + priv.Controller)
+                    .Distinct()
+                    .ToArray();
+
+                foreach (String controller in controllers)
+                    Assert.True(!String.IsNullOrEmpty(manager.GetString(controller)),
+                        String.Format("Privilege controller '{0}', does not have a title.", controller));
+            }
+        }
+
+        [Fact]
+        public void Resources_HasAllPrivilegeActionTitles()
+        {
+            ResourceManager manager = MvcTemplate.Resources.Privilege.Action.Titles.ResourceManager;
+            using (Context context = new Context())
+            {
+                String[] actions = context
+                    .Set<Privilege>()
+                    .ToArray()
+                    .Select(priv => priv.Area + priv.Controller + priv.Action)
+                    .Distinct()
+                    .ToArray();
+
+                foreach (String action in actions)
+                    Assert.True(!String.IsNullOrEmpty(manager.GetString(action)),
+                        String.Format("Privilege action '{0}', does not have a title.", action));
+            }
+        }
+
+        [Fact]
+        public void Resources_HasAllTranslations()
+        {
+            IEnumerable<Language> languages = GlobalizationProviderFactory.CreateProvider().Languages;
+            IEnumerable<Type> resourceTypes = Assembly
+                .Load("MvcTemplate.Resources")
+                .GetTypes()
+                .Where(type => type.Namespace.StartsWith("MvcTemplate.Resources."));
+
+            foreach (Type type in resourceTypes)
+            {
+                ResourceManager manager = new ResourceManager(type);
+                IEnumerable<String> resourceKeys = new String[0];
+
+                foreach (ResourceSet set in languages.Select(language => manager.GetResourceSet(language.Culture, true, true)))
+                {
+                    resourceKeys = resourceKeys.Union(set.Cast<DictionaryEntry>().Select(resource => resource.Key.ToString()));
+                    resourceKeys = resourceKeys.Distinct();
+                }
+
+                foreach (Language language in languages)
+                {
+                    ResourceSet set = manager.GetResourceSet(language.Culture, true, true);
+                    foreach (String key in resourceKeys)
+                        Assert.True(!String.IsNullOrEmpty(set.GetString(key)),
+                            String.Format("{0}, does not have translation for '{1}' in {2} language.",
+                                type.FullName, key, language.Culture.EnglishName));
+                }
+            }
+        }
+    }
+}
