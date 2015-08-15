4e80a5b0ad11f8b21702154dab8eaad05c086c5a
Muchiachio <aldis.rameika@gmail.com>
2015-08-15 19:03:16 +0300

    Shared header resources
    - Merged same header resources to one, to decrease time spent on writing
    resource files, because most of the time headers are the same and if
    they are not they can be extended back.

--- src/MvcTemplate.Resources/Shared/Headers.Designer.cs
+++ src/MvcTemplate.Resources/Shared/Headers.Designer.cs
@@ -63,27 +63,9 @@ namespace MvcTemplate.Resources.Shared {
         /// <summary>
         ///   Looks up a localized string similar to Account.
         /// </summary>
-        public static string AdministrationAccountsCreate {
+        public static string AdministrationAccounts {
             get {
-                return ResourceManager.GetString("AdministrationAccountsCreate", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Account.
-        /// </summary>
-        public static string AdministrationAccountsDetails {
-            get {
-                return ResourceManager.GetString("AdministrationAccountsDetails", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Account.
-        /// </summary>
-        public static string AdministrationAccountsEdit {
-            get {
-                return ResourceManager.GetString("AdministrationAccountsEdit", resourceCulture);
+                return ResourceManager.GetString("AdministrationAccounts", resourceCulture);
             }
         }
         
@@ -99,36 +81,9 @@ namespace MvcTemplate.Resources.Shared {
         /// <summary>
         ///   Looks up a localized string similar to Role.
         /// </summary>
-        public static string AdministrationRolesCreate {
+        public static string AdministrationRoles {
             get {
-                return ResourceManager.GetString("AdministrationRolesCreate", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Role.
-        /// </summary>
-        public static string AdministrationRolesDelete {
-            get {
-                return ResourceManager.GetString("AdministrationRolesDelete", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Role.
-        /// </summary>
-        public static string AdministrationRolesDetails {
-            get {
-                return ResourceManager.GetString("AdministrationRolesDetails", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Role.
-        /// </summary>
-        public static string AdministrationRolesEdit {
-            get {
-                return ResourceManager.GetString("AdministrationRolesEdit", resourceCulture);
+                return ResourceManager.GetString("AdministrationRoles", resourceCulture);
             }
         }
         
@@ -144,18 +99,9 @@ namespace MvcTemplate.Resources.Shared {
         /// <summary>
         ///   Looks up a localized string similar to Profile.
         /// </summary>
-        public static string ProfileDelete {
+        public static string Profile {
             get {
-                return ResourceManager.GetString("ProfileDelete", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Profile.
-        /// </summary>
-        public static string ProfileEdit {
-            get {
-                return ResourceManager.GetString("ProfileEdit", resourceCulture);
+                return ResourceManager.GetString("Profile", resourceCulture);
             }
         }
     }
