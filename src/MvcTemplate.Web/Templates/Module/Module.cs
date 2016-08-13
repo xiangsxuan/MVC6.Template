b02d223b786b0ebef93efa2050ef610dab5dcaa7
Muchiachio <aldis.rameika@gmail.com>
2016-08-13 12:48:30 +0300

    Project module scaffolding
    - Added support for scaffolding basic module controls and it's views.

---
+++ src/MvcTemplate.Web/Templates/Module/Module.cs
@@ -0,0 +1,71 @@
+﻿using Genny;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcTemplate.Web.Templates
+{
+    [GennyModuleDescriptor("Default system module template")]
+    public class Module : GennyModule
+    {
+        [GennyParameter(0, Required = true)]
+        public String Model { get; set; }
+
+        [GennyParameter(1, Required = true)]
+        public String Controller { get; set; }
+
+        [GennyParameter(2, Required = false)]
+        public String Area { get; set; }
+
+        public Module(IServiceProvider provider)
+            : base(provider)
+        {
+        }
+
+        public override void Run()
+        {
+            String path = (Area != null ? Area + "/" : "") + Controller;
+            Dictionary<String, GennyScaffoldingResult> results = new Dictionary<String, GennyScaffoldingResult>();
+            /*
+            results.Add($"../MvcTemplate.Controllers/{path}/{Controller}Controller.cs", Scaffold("Controllers/Controller"));
+            results.Add($"../../test/MvcTemplate.Tests/Unit/Controllers/{path}/{Controller}Tests.cs", Scaffold("Tests/ControllerTests"));
+
+            results.Add($"../MvcTemplate.Objects/Models/{path}/{Model}.cs", Scaffold("Objects/Model"));
+            results.Add($"../MvcTemplate.Objects/Views/{path}/{Model}View.cs", Scaffold("Objects/View"));
+
+            results.Add($"../MvcTemplate.Services/{path}/{Model}Service.cs", Scaffold("Services/Service"));
+            results.Add($"../MvcTemplate.Services/{path}/I{Model}Service.cs", Scaffold("Services/IService"));
+            results.Add($"../../test/MvcTemplate.Tests/Unit/Services/{path}/{Model}ServiceTests.cs", Scaffold("Tests/ServiceTests"));
+
+            results.Add($"../MvcTemplate.Validators/{path}/{Model}Validator.cs", Scaffold("Validators/Validator"));
+            results.Add($"../MvcTemplate.Validators/{path}/I{Model}Validator.cs", Scaffold("Validators/IValidator"));
+            results.Add($"../../test/MvcTemplate.Tests/Unit/Validators/{path}/{Model}ValidatorTests.cs", Scaffold("Tests/ValidatorTests"));
+            */
+            results.Add($"../MvcTemplate.Web/Views/{path}/Index.cshtml", Scaffold("Web/Index"));
+            results.Add($"../MvcTemplate.Web/Views/{path}/Create.cshtml", Scaffold("Web/Create"));
+            results.Add($"../MvcTemplate.Web/Views/{path}/Details.cshtml", Scaffold("Web/Details"));
+            results.Add($"../MvcTemplate.Web/Views/{path}/Edit.cshtml", Scaffold("Web/Edit"));
+            results.Add($"../MvcTemplate.Web/Views/{path}/Delete.cshtml", Scaffold("Web/Delete"));
+
+            if (results.Any(result => result.Value.Errors.Any()))
+            {
+                foreach (GennyScaffoldingResult result in results.Select(x => x.Value))
+                    foreach (String error in result.Errors)
+                        Logger.Write($"    {error}");
+
+                Logger.Write("Scaffolding failed! Rolling back...");
+            }
+            else
+            {
+                TryWrite(results);
+
+                Logger.Write("Scaffolded successfully!");
+            }
+        }
+
+        private GennyScaffoldingResult Scaffold(String path)
+        {
+            return Scaffolder.Scaffold("Templates/Module/" + path, new ModuleModel(Model, Controller, Area));
+        }
+    }
+}
---
+++ src/MvcTemplate.Web/Templates/Module/ModuleModel.cs
@@ -0,0 +1,70 @@
+﻿using MvcTemplate.Objects;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MvcTemplate.Web.Templates
+{
+    public class ModuleModel
+    {
+        public String Model { get; }
+        public String ModelVarName { get; }
+        public String ModelShortName { get; }
+
+        public String View { get; }
+
+        public String Service { get; }
+        public String IService { get; }
+        public String ServiceTests { get; }
+
+        public String Validator { get; }
+        public String IValidator { get; }
+        public String ValidatorTests { get; }
+
+        public String ControllerTestsNamespace { get; }
+        public String ControllerNamespace { get; }
+        public String ControllerTests { get; }
+        public String Controller { get; }
+
+        public String Area { get; }
+
+        public PropertyInfo[] Properties { get; set; }
+        public PropertyInfo[] AllProperties { get; set; }
+
+        public ModuleModel(String model, String controller, String area)
+        {
+            ModelShortName = Regex.Split(model, "(?=[A-Z])").Last();
+            ModelVarName = ModelShortName.ToLower();
+            Model = model;
+
+            View = $"{Model}View";
+
+            ServiceTests = $"{Model}ServiceTests";
+            IService = $"I{Model}Service";
+            Service = $"{Model}Service";
+
+            ValidatorTests = $"{Model}ValidatorTests";
+            IValidator = $"I{Model}Validator";
+            Validator = $"{Model}Validator";
+
+            ControllerTestsNamespace = "MvcTemplate.Tests.Unit.Controllers" + (!String.IsNullOrWhiteSpace(area) ? $".{area}" : "");
+            ControllerNamespace = "MvcTemplate.Controllers" + (!String.IsNullOrWhiteSpace(area) ? $".{area}" : "");
+            ControllerTests = $"{controller}ControllerTests";
+            Controller = $"{controller}Controller";
+
+            Area = area;
+
+            AllProperties = typeof(BaseModel)
+                .Assembly
+                .GetTypes()
+                .SingleOrDefault(type => type.Name.Equals(Model, StringComparison.OrdinalIgnoreCase))?
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance) ?? typeof(BaseModel).GetProperties();
+            Properties = typeof(BaseModel)
+                .Assembly
+                .GetTypes()
+                .SingleOrDefault(type => type.Name.Equals(Model, StringComparison.OrdinalIgnoreCase))?
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly) ?? new PropertyInfo[0];
+        }
+    }
+}
