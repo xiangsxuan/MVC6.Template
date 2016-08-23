57727f0e5ecb50a2c4a0ede0108f6a8bbf5d0c6d
Muchiachio <aldis.rameika@gmail.com>
2016-08-23 15:32:47 +0300

    Project module scaffolding
    - Added missing module controls.

--- src/MvcTemplate.Web/Templates/Module/Module.cs
+++ src/MvcTemplate.Web/Templates/Module/Module.cs
@@ -26,7 +26,7 @@ namespace MvcTemplate.Web.Templates
         {
             String path = (Area != null ? Area + "/" : "") + Controller;
             Dictionary<String, GennyScaffoldingResult> results = new Dictionary<String, GennyScaffoldingResult>();
-            /*
+
             results.Add($"../MvcTemplate.Controllers/{path}/{Controller}Controller.cs", Scaffold("Controllers/Controller"));
             results.Add($"../../test/MvcTemplate.Tests/Unit/Controllers/{path}/{Controller}Tests.cs", Scaffold("Tests/ControllerTests"));
 
@@ -40,7 +40,7 @@ namespace MvcTemplate.Web.Templates
             results.Add($"../MvcTemplate.Validators/{path}/{Model}Validator.cs", Scaffold("Validators/Validator"));
             results.Add($"../MvcTemplate.Validators/{path}/I{Model}Validator.cs", Scaffold("Validators/IValidator"));
             results.Add($"../../test/MvcTemplate.Tests/Unit/Validators/{path}/{Model}ValidatorTests.cs", Scaffold("Tests/ValidatorTests"));
-            */
+
             results.Add($"../MvcTemplate.Web/Views/{path}/Index.cshtml", Scaffold("Web/Index"));
             results.Add($"../MvcTemplate.Web/Views/{path}/Create.cshtml", Scaffold("Web/Create"));
             results.Add($"../MvcTemplate.Web/Views/{path}/Details.cshtml", Scaffold("Web/Details"));
