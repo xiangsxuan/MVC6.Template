b0d4d9191f322c56b942aec6d326c04b3b8a8a5f
Muchiachio <aldis.rameika@gmail.com>
2016-11-03 08:03:32 +0200

    Role creation
    - Fixed, role creation page crashing, because of null permissions.

--- src/MvcTemplate.Services/Administration/Roles/RoleService.cs
+++ src/MvcTemplate.Services/Administration/Roles/RoleService.cs
@@ -22,6 +22,7 @@ namespace MvcTemplate.Services
 
         public virtual void SeedPermissions(RoleView view)
         {
+            view.Permissions = view.Permissions ?? new JsTree();
             JsTreeNode root = new JsTreeNode(Titles.All);
             view.Permissions.Nodes.Add(root);
 
