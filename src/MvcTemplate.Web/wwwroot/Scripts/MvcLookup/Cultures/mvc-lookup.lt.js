c55e7c0b6ddb6a871d0fd9f019d05c26aaf44c79
Muchiachio <aldis.rameika@gmail.com>
2016-12-14 11:26:15 +0200

    Correct lookup localization
    - Fixed, lookup localization using old lookup version format.

--- src/MvcTemplate.Web/wwwroot/Scripts/MvcLookup/Cultures/mvc-lookup.lt.js
+++ src/MvcTemplate.Web/wwwroot/Scripts/MvcLookup/Cultures/mvc-lookup.lt.js
@@ -1,7 +1,7 @@
 ﻿window.cultures = window.cultures || {};
 window.cultures.lookup = window.cultures.lookup || {};
 window.cultures.lookup['lt'] = {
-    Error: "Įvyko klaida, susisiekite su administratoriumi",
-    NoDataFound: "Irašų nerasta",
-    Search: "Ieškoti..."
+    error: "Įvyko klaida, susisiekite su administratoriumi",
+    noData: "Irašų nerasta",
+    search: "Ieškoti..."
 };
