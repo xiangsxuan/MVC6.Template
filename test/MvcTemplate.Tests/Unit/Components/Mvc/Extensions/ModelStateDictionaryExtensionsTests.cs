eb5cf62ffd4f32457de927697e6f655e05c05d85
Muchiachio <aldis.rameika@gmail.com>
2017-08-05 17:30:10 +0300

    Model state errors for json based requests
    - Added support for getting model state error dictionary for use in
    unobtrusive validation.

---
+++ test/MvcTemplate.Tests/Unit/Components/Mvc/Extensions/ModelStateDictionaryExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MvcTemplate.Components.Mvc;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MvcTemplate.Tests.Unit.Components.Mvc
+{
+    public class ModelStateDictionaryExtensionsTests
+    {
+        private ModelStateDictionary modelState;
+
+        public ModelStateDictionaryExtensionsTests()
+        {
+            modelState = new ModelStateDictionary();
+        }
+
+        #region Errors(this ModelStateDictionary modelState)
+
+        [Fact]
+        public void Errors_FromModelState()
+        {
+            modelState.AddModelError("Empty", "");
+            modelState.AddModelError("Error", "Error");
+            modelState.AddModelError("EmptyErrors", "");
+            modelState.AddModelError("EmptyErrors", "E");
+            modelState.SetModelValue("NoErrors", "A", "A");
+            modelState.AddModelError("TwoErrors", "Error1");
+            modelState.AddModelError("TwoErrors", "Error2");
+            modelState.AddModelError("WhitespaceErrors", "       ");
+            modelState.AddModelError("WhitespaceErrors", "Whitespace");
+
+            Dictionary<String, String> actual = modelState.Errors();
+            Dictionary<String, String> expected = new Dictionary<String, String>
+            {
+                ["Empty"] = null,
+                ["Error"] = "Error",
+                ["EmptyErrors"] = "E",
+                ["TwoErrors"] = "Error1",
+                ["WhitespaceErrors"] = "       "
+            };
+
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+    }
+}
