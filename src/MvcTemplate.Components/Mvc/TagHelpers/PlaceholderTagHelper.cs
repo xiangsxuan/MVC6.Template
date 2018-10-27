ac58eb6ebe3f1e16e8a9d0957cf126a12018fa43
Muchiachio <aldis.rameika@gmail.com>
2018-10-27 14:35:06 +0300

    Placeholder tag helper
    - Added support for using automatic input placeholders through a tag helpers.

---
+++ src/MvcTemplate.Components/Mvc/TagHelpers/PlaceholderTagHelper.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace MvcTemplate.Components.Mvc
+{
+    [HtmlTargetElement("input", Attributes = "asp-for,asp-placeholder")]
+    public class PlaceholderTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("placeholder", For.Metadata.DisplayName);
+        }
+    }
+}
---
+++ test/MvcTemplate.Tests/Unit/Components/Mvc/TagHelpers/PlaceholderTagHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using MvcTemplate.Components.Mvc;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace MvcTemplate.Tests.Unit.Components.Mvc
+{
+    public class PlaceholderTagHelperTests
+    {
+        #region Process(TagHelperContext context, TagHelperOutput output)
+
+        [Fact]
+        public void Process_Placeholder()
+        {
+            ModelMetadata metadata = Substitute.For<ModelMetadata>(ModelMetadataIdentity.ForType(typeof(String)));
+            TagHelperOutput output = new TagHelperOutput("input", new TagHelperAttributeList(), (useCache, encoder) => null);
+            PlaceholderTagHelper helper = new PlaceholderTagHelper { For = new ModelExpression("Total", new ModelExplorer(new EmptyModelMetadataProvider(), metadata, null)) };
+
+            metadata.DisplayName.Returns("Test");
+
+            helper.Process(null, output);
+
+            Assert.Single(output.Attributes);
+            Assert.Empty(output.Content.GetContent());
+            Assert.Equal("Test", output.Attributes["placeholder"].Value);
+        }
+
+        #endregion
+    }
+}
